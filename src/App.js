baf0eee4b83160a99e4d378cb8d3a8c8050c382a
PetrosLeonidou <petros.leonidou@tiomarkets.com>
2022-04-20 18:02:09 +0300

    setup the api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [countriesResponse, setCountriesResponse] = useState(null);
+
+  const url = "https://restcountries.com/v2/all";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        setCountriesResponse(res);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {countriesResponse && (
+        <>
+          {countriesResponse.map((item, idx) => (
+            <p key={idx}>{item.name}</p>
+          ))}
+        </>
+      )}
     </div>
   );
 }

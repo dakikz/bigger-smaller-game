e9c3f9e4ccc74ad9b85f7876b471c25410a487d5
dakikz <leonidoupetros@gmail.com>
2022-04-25 22:34:45 +0300

    random country selection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const ComparisonOuter = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const CountryBoxA = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid green;
+  height: 50vh;
+  background-color: #cccccc;
+`;
+const CountryBoxB = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid green;
+  height: 50vh;
+  background-color: #cccccc;
+`;
 
 function App() {
   const [countriesResponse, setCountriesResponse] = useState(null);
+  const [numberA, setNumberA] = useState(0);
+  const [numberB, setNumberB] = useState(0);
 
   const url = "https://restcountries.com/v2/all";
 
@@ -9,19 +35,71 @@ function App() {
     fetch(url)
       .then((response) => response.json())
       .then((res) => {
-        console.log(res);
+        // console.log(res);
         setCountriesResponse(res);
       });
   }, []);
 
+  let randCountry;
+  let randCountry2;
+
+  const randCountryF = () => {
+    randCountry = Math.floor(Math.random() * countriesResponse.length);
+  };
+  const randCountryS = () => {
+    randCountry2 = Math.floor(Math.random() * countriesResponse.length);
+    if (randCountry2 === randCountry) {
+      randCountry2 += 1;
+    }
+  };
+  const more = () => {
+    if (
+      countriesResponse[numberB].population >=
+      countriesResponse[numberA].population
+    ) {
+      console.log("NumA: ", countriesResponse[numberA].population);
+      console.log("NumB: ", countriesResponse[numberB].population);
+      console.log("Win");
+    } else {
+      console.log("Lose");
+    }
+    randCountryS();
+    setNumberA(randCountry);
+    setNumberB(randCountry2);
+  };
+  const less = () => {
+    if (
+      countriesResponse[numberA].population >=
+      countriesResponse[numberB].population
+    ) {
+      console.log("NumA: ", countriesResponse[numberA].population);
+      console.log("NumB: ", countriesResponse[numberB].population);
+      console.log("Win");
+    } else {
+      console.log("Lose");
+    }
+    randCountryS();
+    setNumberA(randCountry);
+    setNumberB(randCountry2);
+  };
+
   return (
     <div className="App">
       {countriesResponse && (
-        <>
-          {countriesResponse.map((item, idx) => (
-            <p key={idx}>{item.name}</p>
-          ))}
-        </>
+        <ComparisonOuter>
+          <CountryBoxA>
+            {randCountryF()}
+            <p>{countriesResponse[numberA].name}</p>
+            <h3>{countriesResponse[numberA].population.toLocaleString()}</h3>
+          </CountryBoxA>
+          <CountryBoxB>
+            {randCountryS()}
+            <p>{countriesResponse[numberB].name}</p>
+            <h3>{countriesResponse[numberB].population.toLocaleString()}</h3>
+            <button onClick={() => more()}>More</button>
+            <button onClick={() => less()}>Less</button>
+          </CountryBoxB>
+        </ComparisonOuter>
       )}
     </div>
   );

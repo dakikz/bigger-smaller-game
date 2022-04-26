14cfb9776db41e6b81a0d620145c0e22a85b1c6e
PetrosLeonidou <petros.leonidou@tiomarkets.com>
2022-04-26 17:57:19 +0300

    restructure the code

--- src/App.js
+++ src/App.js
@@ -1,103 +1,190 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 const ComparisonOuter = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
 `;
 const CountryBoxA = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  border: 1px solid green;
+  border: 1px solid #cccccc;
   height: 50vh;
-  background-color: #cccccc;
+  background-color: #111111;
+  color: #ffffff;
 `;
 const CountryBoxB = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  border: 1px solid green;
+  border: 1px solid #cccccc;
   height: 50vh;
-  background-color: #cccccc;
+  background-color: #111111;
+  color: #ffffff;
+`;
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 180px;
+  & img {
+    max-width: 200px;
+    width: 100%;
+    max-height: 180px;
+    height: auto;
+  }
 `;
 
+const ButtonCustom = styled.button`
+  border: 2px solid #cccccc;
+  background-color: #cccccc;
+  border-radius: 100px;
+  width: 150px;
+  padding: 10px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: transparent;
+    color: #cccccc;
+  }
+`;
 function App() {
   const [countriesResponse, setCountriesResponse] = useState(null);
-  const [numberA, setNumberA] = useState(0);
-  const [numberB, setNumberB] = useState(0);
+  const [checkedCountries, setCheckedCountries] = useState([]);
+  const [numberA, setNumberA] = useState();
+  const [numberB, setNumberB] = useState();
+  const isReady = useRef(false);
 
   const url = "https://restcountries.com/v2/all";
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
+      .catch((err1) => {
+        isReady.current = false;
+      })
       .then((res) => {
         // console.log(res);
         setCountriesResponse(res);
+        let randomNumber = Math.floor(Math.random() * res.length);
+        setNumberA(randomNumber);
+        setNumberB(randomNumber + 1);
+
+        isReady.current = true;
+      })
+      .catch((err) => {
+        console.log(err);
+        isReady.current = true;
       });
   }, []);
 
-  let randCountry;
-  let randCountry2;
-
-  const randCountryF = () => {
-    randCountry = Math.floor(Math.random() * countriesResponse.length);
-  };
-  const randCountryS = () => {
-    randCountry2 = Math.floor(Math.random() * countriesResponse.length);
-    if (randCountry2 === randCountry) {
-      randCountry2 += 1;
+  const less = () => {
+    if (
+      countriesResponse[numberB].population <
+      countriesResponse[numberA].population
+    ) {
+      console.log("win");
+      let newCountry = Math.floor(
+        Math.random() *
+          countriesResponse
+            .map((x) => x.name)
+            .filter((x) => countriesResponse.map((x) => x.name).indexOf(x) > -1)
+            .length
+      );
+      setNumberA(numberB);
+      setNumberB(newCountry);
+      let checkedArray = [...checkedCountries];
+      checkedArray.push(countriesResponse[newCountry].name);
+      setCheckedCountries(checkedArray);
+    } else {
+      console.log("lose");
     }
   };
   const more = () => {
     if (
-      countriesResponse[numberB].population >=
+      countriesResponse[numberB].population >
       countriesResponse[numberA].population
     ) {
-      console.log("NumA: ", countriesResponse[numberA].population);
-      console.log("NumB: ", countriesResponse[numberB].population);
-      console.log("Win");
+      console.log("win");
+      setNumberA(numberB);
+
+      let newCountry = Math.floor(
+        Math.random() *
+          countriesResponse
+            .map((x) => x.name)
+            .filter((x) => countriesResponse.map((x) => x.name).indexOf(x) > -1)
+            .length
+      );
+      setNumberB(newCountry);
+      let checkedArray = [...checkedCountries];
+      checkedArray.push(countriesResponse[newCountry].name);
+      setCheckedCountries(checkedArray);
     } else {
-      console.log("Lose");
+      console.log("lose");
     }
-    randCountryS();
-    setNumberA(randCountry);
-    setNumberB(randCountry2);
-  };
-  const less = () => {
-    if (
-      countriesResponse[numberA].population >=
-      countriesResponse[numberB].population
-    ) {
-      console.log("NumA: ", countriesResponse[numberA].population);
-      console.log("NumB: ", countriesResponse[numberB].population);
-      console.log("Win");
-    } else {
-      console.log("Lose");
-    }
-    randCountryS();
-    setNumberA(randCountry);
-    setNumberB(randCountry2);
+    // console.log(checkedCountries);
   };
 
   return (
     <div className="App">
-      {countriesResponse && (
+      {!isReady.current && <p>Loading...</p>}
+      {isReady.current && (
         <ComparisonOuter>
           <CountryBoxA>
-            {randCountryF()}
-            <p>{countriesResponse[numberA].name}</p>
+            <h2>{countriesResponse[numberA].name}</h2>
+            <p
+              style={{
+                position: "absolute",
+                top: "5px",
+                right: "15px",
+                fontWeight: "600",
+                color: "red",
+              }}
+            >
+              Score: {checkedCountries.length}
+            </p>
+            <ImageContainer>
+              <img
+                src={countriesResponse[numberA].flags.svg}
+                alt={`${countriesResponse[numberA].name} flag`}
+              />
+            </ImageContainer>
+            <p>has</p>
             <h3>{countriesResponse[numberA].population.toLocaleString()}</h3>
+            <p>current population</p>
+            <p
+              style={{
+                position: "absolute",
+                bottom: "-23px",
+                color: "red",
+                fontSize: "35px",
+              }}
+            >
+              VS
+            </p>
           </CountryBoxA>
           <CountryBoxB>
-            {randCountryS()}
-            <p>{countriesResponse[numberB].name}</p>
+            <h2>{countriesResponse[numberB].name}</h2>
+            <ImageContainer>
+              <img
+                src={countriesResponse[numberB].flags.svg}
+                alt={`${countriesResponse[numberA].name} flag`}
+              />
+            </ImageContainer>
+            <p>has</p>
+            <ButtonCustom onClick={more}>More</ButtonCustom>
+            <p>or</p>
+            <ButtonCustom onClick={less}>Less</ButtonCustom>
+            <p>
+              than <strong>{countriesResponse[numberA].name}</strong>
+            </p>
+
             <h3>{countriesResponse[numberB].population.toLocaleString()}</h3>
-            <button onClick={() => more()}>More</button>
-            <button onClick={() => less()}>Less</button>
           </CountryBoxB>
         </ComparisonOuter>
       )}
